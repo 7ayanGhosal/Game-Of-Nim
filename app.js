76aa677d6c5d2e7f03c75f731ddb7478aaf17dad
Ayan Ghosal <ayan.ghosal007@gmail.com>
2020-04-06 15:37:45 +0530

    Inital commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+app.use(express.static("public"));
+
+
+var value = {};
+var len = 3;
+
+app.get('/', function(req, res){
+    value[0] = Math.floor((Math.random()*10)%6+4);
+    value[1] = Math.floor((Math.random()*10)%6+4);
+    value[2] = Math.floor((Math.random()*10)%6+4);
+    res.render("home.ejs",{len: len, value: value});
+})
+
+
+
+app.listen(process.env.PORT||3000, process.env.IP, function(){
+    console.log("server started");
+})
